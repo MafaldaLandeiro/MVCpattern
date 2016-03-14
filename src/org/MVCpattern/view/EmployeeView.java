2f02ce335788e44c6b14ed3ce5f4434f3c70d98f
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-03-14 23:28:19 +0000

    View

---
+++ src/org/MVCpattern/view/EmployeeView.java
@@ -0,0 +1,10 @@
+package org.MVCpattern.view;
+
+public class EmployeeView {
+	public void printEmployee(String name,double salary){
+		System.out.println("---- EMPLOYE VIEW ----");
+		System.out.println("Employee name: "+name);
+		System.out.println("Employe salary: "+ salary);
+	}
+
+}
