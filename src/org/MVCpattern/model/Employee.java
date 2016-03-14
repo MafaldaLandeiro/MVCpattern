117c3ed07109dae45cc954846aeec81f9429e097
MafaldaLandeiro <1110590@isep.ipp.pt>
2016-03-14 23:23:16 +0000

    Model

---
+++ src/org/MVCpattern/model/Employee.java
@@ -0,0 +1,35 @@
+package org.MVCpattern.model;
+
+public class Employee {
+	private String name;
+	
+	private double salary;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public double getSalary() {
+		return salary;
+	}
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+}
